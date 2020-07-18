348afade9b9836373f550aa2b3100d228a978d58
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-18 17:47:09 +0300

    fix(script.js)fix bug

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const checkElem = elem => {
-    if (typeof(elem) === 'string') {
+    if (typeof(elem) === 'string' && isNaN(elem)) {
         elem = elem.trim();
         if (elem.length > 30) {
             console.log(`${elem.slice(0, 30)}...`);
-        } 
+        } else {
+            console.log(elem);
+        }
     } else {
         console.warn('Переданный аргумент не является строкой, пожалуйста исправте аргумент на строку');
     }
 };
 const text = '        Lorem ipsum dolor sit amet consectetur adipisicing elit.       ';
-checkElem(3);
+const textDigitAndString = '     gifjgudf6546656  ';
+const textDigit = '45636636';
+checkElem(text);
+checkElem(textDigitAndString);
+checkElem(textDigit);
